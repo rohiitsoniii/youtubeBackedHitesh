1775db30a727bcd29eef8a556449556d7aed5075
Rohit Soni <rohitsoniahsi001@gmail.com>
2024-07-21 20:42:50 +0530

    file upload

--- src/app.js
+++ src/app.js
@@ -8,10 +8,16 @@ app.use(cors({
     origin: process.env.CORS_ORIGIN,
     Credentials:true
 }))
+
+
 app.use(express.json({
     limit:"16kb"
 }))
+
+
 app.use(express.urlencoded({extended:true, limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+
 export {app} 
